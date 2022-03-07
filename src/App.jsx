6cfbbda7d3ebd85e3a666cd48d73daeb00b067a4
yashrajillusion <95868808+yashrajillusion@users.noreply.github.com>
2022-03-07 10:59:14 +0530

    someChenge

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Inventory } from "./Items/Items";
 import React from "react";
 
 
-let data = [{name:"Books:", classadd:"addBook", classrem:"remBook",classtotal:"totalBooks",count:13},{name:"Pens:", classadd:"addPen", classrem:"remPen",classtotal:"totalPens",count:10},{name:"Notebooks:",count:44, classadd:"addNotebook", classrem:"remNotebook",classtotal:"totalNotebooks"},{name:"InkPens:",count:78, classadd:"addInkPen", classrem:"remInkPen",classtotal:"totalInkPens"}]
+let data = [{name:"Books:", classadd:"addBook", classrem:"remBook",classtotal:"totalBooks",count:13},{name:"Pens:", classadd:"addPen", classrem:"remPen",classtotal:"totalPens",count:10},{name:"Notebooks:",count:44, classadd:"addNotebook", classrem:"remNotebook",classtotal:"totalNotebooks"},{name:"InkPens:",count:78, classadd:"addInkpen", classrem:"remInkpen",classtotal:"totalInkpens"}]
 
 
 function App() {
